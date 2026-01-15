b70ec67ccce29a4f03fe4a95ab89bd4878a0eadf
JorgeLo1 <jeyle222@gmail.com>
2026-01-15 09:26:08 -0500

    chore: Limpieza de archivos

--- pruebas.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.post(
-    "https://chatbot-postgrados-ud.onrender.com/webhooks/rest/webhook",
-    json={"sender": "usuario_prueba", "message": "hola"}
-)
-
-print(response.json())
